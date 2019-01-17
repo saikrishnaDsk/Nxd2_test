5728a09308fb0901c5164025c752f71b2d58eb0e
saikrishnaDsk <saikrishnadosapati@gmail.com>
2019-01-17 12:17:55 +0530

    Login and navbar

---
+++ Homepage/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>NXD2</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="../App/assets/navbar.css">
+        <link rel="stylesheet" type="text/css" href="../App/assets/home.css">
+
+        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <!-- NAVBAR-->    
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <img src="../App/assets/logo1.png" class="navbar-brand" width = "150px" height = "100px" alt = "NXD2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsenavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsenavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navdrop" role="button" data-toggle="dropdown" data-hover="dropdown">Products</a>
+                        <div class="dropdown-menu" aria-labelledby="navdrop">
+                            <a href="#" class="dropdown-item">Digital Boards</a>
+                            <a href="#" class="dropdown-item">Our Software</a>
+                            <a href="#" class="dropdown-item">Creatives</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navdrop" role="button" data-toggle="dropdown" data-hover="dropdown">Services</a>
+                        <div class="dropdown-menu" aria-labelledby="navdrop">
+                            <a href="#" class="dropdown-item">Cloud Hosting</a>
+                            <a href="#" class="dropdown-item">Advertising</a>
+                            <a href="#" class="dropdown-item">Statistics</a>
+                        </div>
+                        
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="features.html" class="nav-link">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="aboutus.html" class="nav-link">AboutUs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../App/index.html" class="nav-link">Sign-up/in</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--END NAVBAR-->
+            
+        <?php
+
+            echo "<h1>NEXT GENERATION DIGITAL BOARDS</h1>";
+
+        ?>
+
+    </body>
+
+</html>
